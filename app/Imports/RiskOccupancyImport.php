bd5abcd059d7c30150ad12377aeeb26f86230bbc
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2023-09-21 13:26:01 +0530

    FEAT: import risk occupancy

--- app/Imports/RiskOccupancyImport.php
+++ app/Imports/RiskOccupancyImport.php
@@ -17,16 +17,33 @@ class RiskOccupancyImport implements ToCollection
     {
         DB::beginTransaction();
         try {
-            foreach ($collection as $row) {
 
-                if (empty($row['iib_code']) || empty($row['risk_occupancy']) || empty($row['risk_code'])) {
-                    throw new \Exception('One or more columns in a row are missing or empty');
+            if (
+                $collection->isEmpty() ||
+                count($collection->first()) !== 3 ||
+                $collection->first()->contains(null)
+            ) {
+                throw new \Exception('The Excel file is invalid !');
+            }
+
+            RiskOccupancy::truncate();
+            $headers = $collection->shift();
+
+
+            foreach ($collection as $row) {
+                $data = [];
+                foreach ($headers as $key => $header) {
+                    $data[$header] = $row[$key];
                 }
+
+                // if (empty($row['iib_code']) || empty($row['risk_occupancy']) || empty($row['risk_code'])) {
+                //     throw new \Exception('One or more columns in a row are missing or empty');
+                // }
                 RiskOccupancy::create([
                     'uuid' => UuidGeneratorHelper::generateUniqueUuidForTable('risk_occupancies'),
-                    'iib_code' => $row['iib_code'],
-                    'risk_occupancy' => $row['risk_occupancy'],
-                    'risk_code' => $row['risk_code'],
+                    'iib_code' => $data['iib_code'],
+                    'risk_occupancy' => $data['risk_occupancy'],
+                    'risk_code' => $data['risk_code'],
                 ]);
             }
 
